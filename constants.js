2dd47dcd248514af9a373ab8d2ecc224dd166399
문경원 <mungyeong-won@mungyeong-won-ui-MacBook-Pro.local>
2018-06-22 18:22:09 +0900

    ...

---
+++ constants.js
@@ -0,0 +1,1 @@
+export const API_URL = "http://63482dda.ngrok.io";
---
+++ screens/LogInScreen/container.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import LogInScreen from "./presenter";
+import { Alert } from "react-native";
+
+class Container extends Component {
+      state = {
+        username: "",
+        password: "",
+        isSubmitting: false
+      };
+  render() {
+        return (
+            <LogInScreen
+              {...this.state}
+              changeUsername={this._changeUsername}
+              changePassword={this._changePassword}
+              submit={this._submit}
+            />
+          );
+  }
+    _changeUsername = text => {
+        this.setState({ username: text });
+      };
+      _changePassword = text => {
+        this.setState({ password: text });
+      };
+      _submit = () => {
+        const { username, password, isSubmitting } = this.state;
+        const { login } = this.props;
+        if (!isSubmitting) {
+          if (username && password) {
+            this.setState({
+              isSubmitting: true
+            });
+            login(username, password);
+          } else {
+            Alert.alert("All fields are required");
+          }
+        }
+      };
+}
+
+export default Container;
